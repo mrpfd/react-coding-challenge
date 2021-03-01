548d986c53fe7867859057fbd8bb9eb370c0f9cf
glenadamson1984 <glenadamson1984@gmail.com>
2021-03-01 20:21:27 +0000

    creating articles component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import './App.scss';
+import { Articles } from './Articles';
 import { Header } from './Header';
 
 function App() {
@@ -13,16 +14,7 @@ function App() {
   return (
     <div className={globalTheme}>
       <Header theme={theme} themeToggler={themeToggler} />
-      
-      <div className="columns">
-        <div className="column">
-          <p>Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends.</p>
-        </div>
-        <div className="column">
-          <p>You think water moves fast? You should see ice. It moves like it has a mind. Like it knows it killed the world once and got a taste for murder. After the avalanche, it took us a week to climb out. Now, I don't know exactly when we turned on each other, but I know that seven of us survived the slide... and only five made it out.</p>
-        </div>
-        {/* Text from https://slipsum.com/ */}
-      </div>
+      <Articles />
 
       <div className="field">
         <div className="control">
